753d3dd3cd01027bb348f04a01bf0ea1cbc67501
Marina Domrachova <apaycheva@gmail.com>
2020-06-25 20:33:03 +0300

    Second part

---
+++ script.js
@@ -0,0 +1,35 @@
+/*let num = Math.floor(Math.random() * 10);
+console.log(num);
+let guess = +prompt("Guess a number");
+
+if (num === guess) {
+  console.log("You are right");
+} else if (num > guess) {
+  console.log("Number is lower");
+} else if (num < guess) {
+  console.log("Number is highter");
+} else {
+  console.log("Try again");
+}
+
+console.log("Game over");*/
+
+/*let trueString = "true";
+let falseString = "false";
+
+console.log(Boolean(trueString), Boolean(falseString));*/
+
+let num = 18;
+console.log(num);
+let age = +prompt("What is your age?");
+
+if (isNan() num === age) {
+  console.log("Allow");
+} else if (num < age) {
+  console.log("Allow");
+} else {
+  console.log("Forbidden");
+}
+
+console.log("Game over");
+
